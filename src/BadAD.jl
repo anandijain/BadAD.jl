711be1a6c4c78abc37f35e96251f13c2075ab97f
anandijain <anandj@uchicago.edu>
2020-11-30 17:17:15 -0800

    fixed quotient rule bug, added more primatives, some tests

--- src/BadAD.jl
+++ src/BadAD.jl
@@ -1,5 +1,7 @@
 module BadAD
 
+using StaticArrays
+
 struct Dual{T}
     x::T # primal ?
     der::T # derivative
@@ -19,32 +21,53 @@ Base.:+(a::Number, b::Dual) = b + a
 Base.:-(a::Dual, b::Dual) = Dual(a.x - b.x, a.der - b.der)
 
 Base.:-(a::Dual, b::Number) = Dual(a.x - b, a.der)
-Base.:-(a::Number, b::Dual) = b - a
+Base.:-(a::Number, b::Dual) = Dual(a - b.x, -b.der)
 
 Base.:-(a::Dual) = Dual(-a.x, -a.der)
+# Base.:inv(a::Dual) = Dual(1/a.x, -a.der)
 
+Base.:*(a::Dual, b::Dual) = Dual(a.x * b.x, a.der * b.x + a.x * b.der) # product rule
 
-Base.:*(a::Dual, b::Dual) = Dual(a.x * b.x, a.der*b.x + a.x*b.der) # product rule
-
-Base.:*(a::Dual, b::Number) = Dual(a.x * b, a.der * b) # product rule
+Base.:*(a::Dual, b::Number) = Dual(a.x * b, a.der * b)
 Base.:*(a::Number, b::Dual) = b * a 
 
 Base.:/(a::Dual, b::Dual) = Dual(a.x / b.x, (a.der * b.x - a.x * b.der) / (b.x ^ 2))
 Base.:/(a::Dual, b::Number) = a * inv(b)
-Base.:/(a::Number, b::Dual) = Dual(a * b.x, a * b.der)
+Base.:/(a::Number, b::Dual) = Dual(a / b.x, ( -a * b.der ) / ( b.x ^ 2 ) ) # 
 
-Base.:^(a::Dual, b::Integer) = Base.power_by_squaring(a, b) # the power rule falls out of this 
+Base.:^(a::Dual, b::Integer) = Base.power_by_squaring(a, b) 
+Base.:^(a::Number, b::Dual) = Dual(Float64(a^b.x), a^b.x * log(a))
+
+Base.:≈(a::Dual, b::Dual) = a.x ≈ b.x && a.der ≈ b.der
 
 derivative(f, x) = f(Dual(x, one(x))).der
 
 # primatives
 Base.sin(a::Dual) = Dual(sin(a.x), a.der * cos(a.x))
 Base.cos(a::Dual) = Dual(cos(a.x), -a.der * sin(a.x))
+Base.tan(a::Dual) = sin(a)/cos(a)
+
+Base.csc(a::Dual) = Dual(-csc(a.x)*cot(a.x), -a.der * sin(a.x))
+Base.sec(a::Dual) = Dual(tan(a.x), -a.der * sin(a.x))
+Base.cot(a::Dual) = 1/tan(a)
+
 Base.exp(a::Dual) = Dual(exp(a.x), exp(a.x) * a.der) 
+Base.log(a::Dual) = Dual(log(a.x), a.der/a.x)
 
-# we can now take derivative of the sigmoid function 
-σ(a) = 1/(1+exp(a)) # derivative(σ, 1) ≈ ℯ 
+# used with ∇ and N-dimensional scalar valued functions
+# struct MultiDual{N, T}
+#     x::T
+#     ders::SVector{N, T}
+# end
 
+# Base.:+(a::MultiDual, b::MultiDual) = MultiDual(a.x + b.x, a.ders .+ b.ders)
+# Base.:*(a::MultiDual, b::MultiDual) = MultiDual(a.x * b.x, a.ders .* b.x .+ a.x .* b.ders)
 
+# ∇(f, x) = f(MultiDual(x, ones(length(x)))).ders
+
+# struct MultiDual2{M, N, T}
+#     x::SVector{M, T}
+#     J::SMatrix{M, N, T}
+# end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,26 @@
 using BadAD
+using ForwardDiff
 using Test
 
 @testset "BadAD.jl" begin
-    # Write your tests here.
+
+    a = b = Dual(1, 1)
+
+    S(x) = 1/(1 + exp(-x))
+    dS(x) = exp(-x)/(1 + exp(-x))^2
+
+    f(x) = x^2
+    g(x) = sin(x)
+    h(x) = f(x)/g(x)
+
+    f2(x) = 3^x
+
+    @test -1 * (a + b) == -a + -b
+    @test derivative(g ∘ f, 3.14) == 2*3.14*cos(3.14^2)
+    @test derivative(h, 3) == (2*3*sin(3) - 3^2 * cos(3))/(sin(3)^2)
+    @test derivative(S, 1) == dS(1)
+    @test ForwardDiff.derivative(x->3.5^x, 3) ≈ derivative(x->3.5^x, 3)
+
+
+    f3(x) = SA[log(x[1])]
 end
